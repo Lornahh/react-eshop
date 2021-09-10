7dfe6c9882506133fff7f53a10295b473f2a0870
VeL0citay <31633929+VeL0citay@users.noreply.github.com>
2021-09-10 15:55:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,17 +8,6 @@ const app = express();
 
 app.use(express.json());
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCnDBnxSO-MiLlqqLNglRkdbaC63PQAnqk",
-  authDomain: "react-eshop-68a9c.firebaseapp.com",
-  databaseURL: "https://react-eshop-68a9c.firebaseio.com",
-  projectId: "react-eshop-68a9c",
-  storageBucket: "react-eshop-68a9c.appspot.com",
-  messagingSenderId: "30817821077",
-  appId: "1:30817821077:web:5b65b3cdbb286de3fe969c",
-  measurementId: "G-ZKY8Y88ZXD"
-};
-
 const PORT = 5000 || process.env.PORT;
 
 app.get("/ShopList", (req, res) => {
